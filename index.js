f0fdebc798c35e850edd746552e63da135711658
matthew rendely <matthew.rendely@gmail.com>
2023-02-20 14:28:46 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+function bindKeydown() {
+  document.addEventListener('keydown', function (e) {
+    switch (e.key) {
+      case 'ArrowLeft':
+        moveDodgerLeft();
+        break;
+      case 'ArrowRight':
+        moveDodgerRight();
+        break;
+    }
+
+  });
+}
+
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger');
+  const left_num = parseInt(dodger.style.left);
+  if (left_num > 0) {
+    dodger.style.left = `${left_num - 10}px`;
+  }
+};
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  const left_num = parseInt(dodger.style.left);
+  const parent_width = parseInt(document.getElementById('game').offsetWidth);
+  const dodger_width = parseInt(dodger.offsetWidth);
+  if (left_num < 400 - 40) {
+    dodger.style.left = `${left_num + 10}px`;
+  }
+};
+
+bindKeydown()
